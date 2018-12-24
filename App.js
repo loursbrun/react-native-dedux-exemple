e79e953b93efb4a972ded9500af5f1c6dbbad9b1
fbrun <fabien.brun@kcitylabs.fr>
2018-12-24 15:25:29 +0100

    add redux

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import { AppRegistry, StyleSheet, View, StatusBar } from "react-native";
+
+import itemApp from "./reducers";
+import Header from "./components/Header";
+import ItemList from "./components/ItemList";
+
+// Create store with logger.
+// Use Cmd + D to open developer menu to start debugger
+const store = createStore(itemApp, applyMiddleware(logger));
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // Set status bar text to white
+    StatusBar.setHidden(false);
+    StatusBar.setBarStyle("light-content");
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Header />
+          <ItemList />
+        </View>
+      </Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
